723ea05942555de91db2a376a471a14e03fa9ccc
shaolix <li.shao@mail.utoronto.ca>
2017-04-08 12:35:38 -0400

    change database 2

--- backend_js/mongo.js
+++ backend_js/mongo.js
@@ -5,7 +5,7 @@ var _db;
 module.exports = {
 
   connectToServer: function(callback) {
-    MongoClient.connect('mongodb://dotogether:csc309team08@ds147510.mlab.com:47510/dotogether', function(err, db) {
+    MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds157040.mlab.com:57040/yazero', function(err, db) {
       _db = db;
       return callback(err);
     });
@@ -16,3 +16,5 @@ module.exports = {
   }
   
 }; 
+
+/*mongodb://dotogether:csc309team08@ds147510.mlab.com:47510/dotogether*/
