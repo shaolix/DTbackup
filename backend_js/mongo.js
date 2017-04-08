177623a42765f5cafb73634e84f6fa4b743e2ec4
shaolix <li.shao@mail.utoronto.ca>
2017-04-08 12:13:55 -0400

    change database

--- backend_js/mongo.js
+++ backend_js/mongo.js
@@ -5,7 +5,7 @@ var _db;
 module.exports = {
 
   connectToServer: function(callback) {
-    MongoClient.connect('mongodb://localhost:27017/dotogether', function(err, db) {
+    MongoClient.connect('mongodb://dotogether:csc309team08@ds147510.mlab.com:47510/dotogether', function(err, db) {
       _db = db;
       return callback(err);
     });
